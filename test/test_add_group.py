3d831025de401719a93a60d69bce24bc77eae207
Denis <vsekrasiva@gmail.com>
2022-02-26 15:17:34 +0300

    session class created

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,15 +12,15 @@ def app(request):                 #инициализируем фикстуру
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="test", header="test", footer="test"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
