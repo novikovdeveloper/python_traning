5c6162842b85d2d615c3953fbefa66d79805ce76
Denis <vsekrasiva@gmail.com>
2022-03-27 21:23:03 +0300

    homework_17_edited

--- conftest.py
+++ conftest.py
@@ -19,7 +19,7 @@ def app(request):
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
         with open(config_file) as f:
             target = json.load(f)
-    if fixture is None or fixture.is_valid():
+    if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target["baseUrl"])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
