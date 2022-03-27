50a8281592c97e5ee9d2df764d1df44793532bb1
Denis <vsekrasiva@gmail.com>
2022-03-27 21:07:55 +0300

    json_load_files

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import json
 import pytest
 import os.path
+import importlib
+import jsonpickle
 from fixture.application import Application
 
 
@@ -36,3 +38,24 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+# позволяет получить полную информацию о тестовой функции (напр. фикстурах)
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+# загрузка данных из модуля с заданным именем
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
