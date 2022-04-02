28d73c3b29c596da318d172bc34b2439f424493e
Denis <vsekrasiva@gmail.com>
2022-04-02 18:47:57 +0300

    Ограничения или условия отбора загружаемых данных

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,13 +1,13 @@
 # создан файл для связи с БД и извлечения данных из нее
-import pymysql.cursors
+from fixture.db import DbFixture
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 # извлечем информацию из таблицы group_list в виде набора строк
 try:
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM group_list")
-    for row in cursor.fetchall():
-        print(row)
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
-    connection.close()
+    db.destroy()
--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -11,6 +12,7 @@ class DbFixture:
         self.password = password
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
 
+# метод для загрузки групп
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
@@ -22,6 +24,20 @@ class DbFixture:
         finally:
             cursor.close()
         return list
+
+# метод для загрузки контактов
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT id, firstname, lastname FROM addressbook WHERE deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
+
 
     def destroy(self):
         self.connection.close()
