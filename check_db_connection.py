77243daadbcc9304308068f5143f9c53769a4657
Denis <vsekrasiva@gmail.com>
2022-04-03 20:02:49 +0300

    Отношения между обьектами

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
 # создан файл для связи с БД и извлечения данных из нее
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
-# извлечем информацию из таблицы group_list в виде набора строк
+# извлечем информацию из таблицы в виде набора строк
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="138"))
     for item in l:
         print(item)
     print(len(l))
