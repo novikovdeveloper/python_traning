b06c1020ab2695442faf5566dcac68d58ddf29cf
Denis <vsekrasiva@gmail.com>
2022-04-04 10:50:01 +0300

    homework 21

--- test/test_data_on_home_page.py
+++ test/test_data_on_home_page.py
@@ -1,5 +1,6 @@
 import re
 from random import randrange
+from model.contact import Contact
 
 
 def test_some_data_on_contacts_page(app):
@@ -31,3 +32,11 @@ def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             filter(lambda x: x is not None,
                                    [contact.email, contact.email2, contact.email3])))
+
+
+def test_assert_all_contacts_from_app_and_db(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    ui_contacts = sorted(contacts_from_home_page, key=Contact.id_or_max)
+    db_contacts = sorted(contact_from_db, key=Contact.id_or_max)
+    assert ui_contacts == db_contacts
