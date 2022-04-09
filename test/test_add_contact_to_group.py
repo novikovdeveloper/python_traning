57bcbea1c57c27882b72631f67b73f0b80a63e05
Denis <vsekrasiva@gmail.com>
2022-04-09 12:05:59 +0300

    homework 22 qstn

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -3,24 +3,24 @@ from model.group import Group
 
 
 def test_add_contact_to_group(app, db):
-    #проверка наличия групп, если их нет, то добавляем
+    # проверка наличия групп, если их нет, то добавляем
     groups = db.get_group_list()
     if len(groups) == 0:
         app.group.create(Group(name='test'))
         groups = db.get_group_list()
-    #проверка наличия контактов, не добавленных в группы
-    if len(check_free_contact(groups)) == 0:
+    # проверка наличия контактов, не добавленных в группы
+    if len(check_free_contact(groups, db)) == 0:
         app.contact.create(Contact(firstname='test'))
-    free_contacts = check_free_contact(groups)
+    free_contacts = check_free_contact(groups, db)
     free_contact_in_group = free_contacts[0]
     group_id = free_contact_in_group[1]
     free_contacts = free_contact_in_group[0]
     free_contact = free_contacts[0]
     app.contact.add_contact_to_group(free_contact.id, group_id)
-    #group = db.get_group(Group(id=group_id))
-    #проверка добаления контакта в группу
+    # group = db.get_group(Group(id=group_id))
+    # проверка добаления контакта в группу
     contacts_in_group = db.get_contacts_in_group(Group(id=group_id))[0]
-    list_of_id=[]
+    list_of_id = []
     for item in contacts_in_group:
         list_of_id.append(item.id)
     assert free_contact.id in list_of_id
--- test/test_remove_contact_from_grpup.py
+++ test/test_remove_contact_from_grpup.py
@@ -9,12 +9,12 @@ def test_del_contact_from_group(app, db):
         app.group.create(Group(name='test'))
         groups = db.get_group_list()
     #сначала проверим, что существует хоть один несвободный контакт
-    if len(check_not_free_contact(groups)) == 0:
+    if len(check_not_free_contact(groups, db)) == 0:
         app.contact.create(Contact(firstname='test'))
         new_contacts = db.get_contact_list()
         contact = max(new_contacts, key=Contact.id_or_max)
         app.contact.add_contact_in_group(contact.id, groups[0].id)
-    not_free_contacts = check_not_free_contact(groups)
+    not_free_contacts = check_not_free_contact(groups, db)
     not_free_contact_in_group = not_free_contacts[0]
     group_id = not_free_contact_in_group[1]
     not_free_contacts = not_free_contact_in_group[0]
