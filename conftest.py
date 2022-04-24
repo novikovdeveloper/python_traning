7fe1bcb679f250009f003dfe4fcb7cd30da95be8
Denis <vsekrasiva@gmail.com>
2022-04-24 12:33:05 +0300

    BDD

--- conftest.py
+++ conftest.py
@@ -40,7 +40,7 @@ def db(request):
     return dbfixture
 
 
-@pytest.fixture(scope="session", autouse=True) #
+@pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
